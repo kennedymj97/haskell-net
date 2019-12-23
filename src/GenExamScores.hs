2fc9b4f58c9f2cf2e6efc135cb3caf98610ded6e
Matthew Kennedy <kennedymj97@gmail.com>
2019-12-23 18:32:43 +0000

    added functionality to run epochs and view results after each epoch

---
+++ src/GenExamScores.hs
@@ -0,0 +1,25 @@
+module GenExamScores where
+
+import System.Random
+import Control.Monad
+
+main :: Int -> String -> IO ()
+main n fname = do
+  results <- genResults n
+  writeFile fname $ unlines results
+
+genResult :: IO String
+genResult = do
+  test1 <- randomRIO (0, 100 :: Double)
+  test2 <- randomRIO (0, 100 :: Double)
+  let adm = isAdm test1 test2
+  return $ show test1 ++ "," ++ show test2 ++ "," ++ show adm
+    where
+      isAdm :: Double -> Double -> Int
+      isAdm test1 test2 | test1 + test2 >= 120 = 1
+                        | test1 >= 80 = 1
+                        | test2 >= 70 = 1
+                        | otherwise = 0
+
+genResults :: Int -> IO [String]
+genResults n = replicateM n genResult
--- src/Network.hs
+++ src/Network.hs
@@ -57,36 +57,22 @@ randomNet :: Int -> [Int] -> Int -> IO Network
 randomNet i [] o     = Output <$> randomConnections i o
 randomNet i (h:hs) o = Layer <$> randomConnections i h <*> randomNet h hs o
 
-fst3 :: (a, b, c) -> a
-fst3 (x, _, _) = x
-
-train ::
-     Double -- Learning rate
-  -> Vector Double --input vector
-  -> Vector Double --target vector
-  -> Network -- network to train
-  -> (Network, Double)
-train lr inp0 target net =
-  let (trainedNet, _, cost) = go inp0 net
-   in (trainedNet, cost)
+backPropOneInput ::
+     Vector Double -> Vector Double -> Network -> (Network, Double)
+backPropOneInput !inp !target !net =
+  let (net', _, cost) = go inp net
+   in (net', cost)
   where
-    go ::
-         Vector Double -- input vector
-      -> Network -- network to train
-      -> (Network, Vector Double, Double)
-    -- handle output layer
-    go !inp (Output c) =
+    go :: Vector Double -> Network -> (Network, Vector Double, Double)
+    go !inp (Output !c) =
       let (z, a) = feedForward c inp
           (c', dWs) = outputErrs a z inp target c
-          c'' = gradientDescent c' lr
-          cost = 1 / 2 * (norm_2 (target - a) ** 2)
-       in (Output c'', dWs, cost)
-    -- handle the inner layers
-    go !inp (c1@(C _ w _ _) `Layer` c2) =
+          cost = norm_2 (target - a) ** 2
+       in (Output c', dWs, cost)
+    go !inp (c1 `Layer` c2) =
       let (z, a) = feedForward c1 inp
           (c1', c2', cost, dWs) = backProp c1 c2 a z inp
-          c1'' = gradientDescent c1' lr
-       in (c1'' `Layer` c2', dWs, cost)
+       in (c1' `Layer` c2', dWs, cost)
     feedForward ::
          Connections -> Vector Double -> (Vector Double, Vector Double)
     feedForward c inp =
@@ -118,67 +104,82 @@ train lr inp0 target net =
           errWs = errs `outer` inp
           dWs = tr w #> errs
        in (C b w (bGrads + errs) (wGrads + errWs), c2', cost, dWs)
-    gradientDescent :: Connections -> Double -> Connections
-    gradientDescent (C b w bGrads wGrads) lr =
-      let b' = b - scale lr bGrads
-          w' = w - scale lr wGrads
-       in C b' w' 0 0
 
-netTest :: Double -> Int -> IO String
-netTest rate n = do
-  inps <-
-    replicateM n $ do
-      s <- randomIO :: IO Int
-      return (randomVector s Uniform 2 * 2 - 1)
-  let outs =
-        flip map inps $ \v ->
-          if v `inCircle` (fromRational 0.33, 0.33) ||
-             v `inCircle` (fromRational (-0.33), 0.33)
-            then fromRational 1
-            else fromRational 0
-  let net0 = randomNet 2 [16, 8] 1
-  trained <- foldl' (trainEach rate) net0 (zip3 inps outs [0 ..])
-  let outMat =
-        [ [ render (norm_2 (runNet trained (vector [x / 25 - 1, y / 10 - 1])))
-        | x <- [0 .. 50]
-        ]
-        | y <- [0 .. 20]
-        ]
-      render r
-        | r <= 0.2 = ' '
-        | r <= 0.4 = '.'
-        | r <= 0.6 = '-'
-        | r <= 0.8 = '='
-        | otherwise = '#'
-  return (unlines outMat)
-  where
-    inCircle :: Vector Double -> (Vector Double, Double) -> Bool
-    v `inCircle` (o, r) = norm_2 (v - o) <= r
+backPropagate ::
+     [Vector Double]
+  -> [Vector Double]
+  -> Double
+  -> Network
+  -> (Network, Double)
+backPropagate !inps !targets !lr !net =
+  let (netFinal, costSum) =
+        foldl'
+          (\(net, cost) (inp, target) ->
+             let (net', cost') = backPropOneInput inp target net
+                 net'' = gradientDescent lr net'
+              in (net'', cost + cost'))
+          (net, 0)
+          (zip inps targets)
+   in (netFinal, costSum / fromIntegral (2 * length inps))
 
-trainEach ::
-     Double -> IO Network -> (Vector Double, Vector Double, Int) -> IO Network
-trainEach rate ntIO (i, o, count) = do
-  nt <- ntIO
-  --putStrLn $ "Input " ++ show count
-  let (net, cost) = train rate i o nt
-  --putStrLn $ "Cost " ++ show cost
-  return net
+gradientDescent :: Double -> Network -> Network
+gradientDescent !lr (Output !c) = Output (applyGrads lr c)
+gradientDescent !lr (c `Layer` n) = applyGrads lr c `Layer` gradientDescent lr n
 
-main :: IO ()
-main = do
-  args <- getArgs
-  let n = readMaybe =<< (args !!? 0)
-      lr = readMaybe =<< (args !!? 1)
-  putStrLn "Training network..."
-  res <- netTest (fromMaybe 0.25 lr) (fromMaybe 500000 n)
-  putStrLn res
+applyGrads :: Double -> Connections -> Connections
+applyGrads !lr (C !b !w !bGrads !wGrads) =
+  C (b - scale lr bGrads) (w - scale lr wGrads) 0 0
 
-(!!?) :: [a] -> Int -> Maybe a
-xs !!? i = listToMaybe (drop i xs)
+runTests :: [Vector Double] -> [Vector Double] -> Double -> Network -> Double
+runTests !testInps !testTargets !thresh !net =
+  let numCorrect =
+        foldl'
+          (\n (inp, target) ->
+             if cmap
+                  (\x ->
+                     if x >= thresh
+                       then 1
+                       else 0)
+                  (runNet net inp) ==
+                target
+               then n + 1
+               else n)
+          0
+          (zip testInps testTargets)
+   in numCorrect / fromIntegral (length testInps)
 
-readExams :: IO ([Vector Double], [Vector Double])
-readExams = do
-  inp <- readFile "./data/ex2data1.txt"
+epoch ::
+     [Vector Double]
+  -> [Vector Double]
+  -> [Vector Double]
+  -> [Vector Double]
+  -> Double
+  -> Network
+  -> (Network, Double, Double)
+epoch !trainInps !trainTargets !testInps !testTargets !lr !net =
+  let (net', cost) = backPropagate trainInps trainTargets lr net
+      accuracy = runTests testInps testTargets 0.5 net
+   in (net', cost, accuracy * 100)
+
+epochIO ::
+     [Vector Double]
+  -> [Vector Double]
+  -> [Vector Double]
+  -> [Vector Double]
+  -> Double
+  -> IO Network
+  -> Int
+  -> IO Network
+epochIO !trainInps !trainTargets !testInps !testTargets !lr !netIO !count = do
+  net <- netIO
+  putStrLn $ "Running epoch " ++ show count ++ "..."
+  let (net', cost, acc) = epoch trainInps trainTargets testInps testTargets lr net
+  putStrLn $ "Cost: " ++ show cost ++ "    Accuracy: " ++ show acc
+  return net'
+
+readExams :: String -> IO ([Vector Double], [Vector Double])
+readExams fpath = do
+  inp <- readFile fpath
   let (inps, outs) = foldr f ([], []) (lines inp)
   return (inps, outs)
   where
@@ -197,16 +198,27 @@ readExams = do
     readAdm :: String -> Double
     readAdm val = read val :: Double
 
-examsNet :: IO Network
+examsNet :: IO ()
 examsNet = do
-  (inps, outs) <- readExams
-  let net0 = randomNet 2 [4] 1
-  trained <-
-    foldr'
-      (\epoch net ->
-         print epoch >> foldl' (trainEach 0.25) net (zip3 inps outs [0 ..]))
-      net0
-      [0 .. 50]
-  print $ runNet trained (vector [34.6, 78])
-  print $ runNet trained (vector [75.5, 90.4])
-  return trained
+  (trainInps, trainOuts) <- readExams "./data/examScoresTrain.txt"
+  (testInps, testOuts) <- readExams "./data/examScoresTest.txt"
+  let net0 = randomNet 2 [8, 8] 1
+  let lr = 0.01
+  let epochIO' = epochIO trainInps trainOuts testInps testOuts lr
+  putStrLn "Training network..."
+  trained <- foldl' epochIO' net0 [0 .. 100]
+  putStrLn "Example results from trained net..."
+  print $ runNet trained (vector [80, 80])
+  print $ runNet trained (vector [20, 30])
+  return ()
+  {-
+  let (trained, cost, acc) = epoch' net0
+  print cost
+  print acc
+  let (trained', cost', acc') = epoch' trained
+  print cost'
+  print acc'
+  let (trained'', cost'', acc'') = epoch' trained'
+  print cost''
+  print acc''
+  -}
