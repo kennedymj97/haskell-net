d7f2d304d4b7f5a2ca66237b10ac5502bf85af9f
Matthew Kennedy <kennedymj97@gmail.com>
2019-12-30 14:45:30 +0000

    refactoring, minor chage to backprop in outer layer

--- src/Network.hs
+++ src/Network.hs
@@ -32,50 +32,18 @@ data Network where
   Output :: !Connections -> Network
   Layer :: !Connections -> !Network -> Network
 
-sigmoid :: Floating a => a -> a
-sigmoid x = 1 / (1 + exp (-x))
-
-sigmoid' :: Floating a => a -> a
-sigmoid' x = sigmoid x * (1 - sigmoid x)
-
-relu :: (Element b, Num b, Container c b, Ord b) => c b -> c b
-relu = cmap (max 0)
-
-relu' :: (Element b, Num b, Container c b, Ord b) => c b -> c b
-relu' =
-  cmap
-    (\x ->
-       if x <= 0
-         then 0
-         else 1)
-
-softmax :: (Element b, Num b, Container c b, Ord b, Floating b) => c b -> c b
-softmax xs = cmap (\x -> exp x / expSum) xs
-  where
-    expSum = sumElements $ cmap exp xs
-
 weightedInput :: Connections -> Vector Double -> Vector Double
 weightedInput (C b w _ _) v = w #> v + b
 
-runNet :: Network -> Vector Double -> Vector Double
-runNet (Output c) !inp = sigmoid (weightedInput c inp)
-runNet (c `Layer` n) !inp =
+runNet ::
+     Network
+  -> Vector Double
+  -> (Vector Double -> Vector Double)
+  -> Vector Double
+runNet (Output c) !inp f = f (weightedInput c inp)
+runNet (c `Layer` n) !inp f =
   let inp' = relu (weightedInput c inp)
-   in runNet n inp'
-
-randomConnections :: Int -> Int -> IO Connections
-randomConnections i o = do
-  seed1 <- randomIO :: IO Int
-  seed2 <- randomIO :: IO Int
-  let b = randomVector seed1 Uniform o * 2 - 1
-      w = uniformSample seed2 o (replicate i (-1, 1))
-  return (C b w 0 0)
-
-kaimingInit :: Int -> Int -> IO Connections
-kaimingInit i o = do
-  let b = vector (replicate o 0)
-  w <- scale (sqrt (2 / fromIntegral i)) <$> randn o i
-  return (C b w 0 0)
+   in runNet n inp' f
 
 randomNet :: Int -> [Int] -> Int -> (Int -> Int -> IO Connections) -> IO Network
 randomNet i [] o initFunc = Output <$> initFunc i o
@@ -83,22 +51,29 @@ randomNet i (h:hs) o initFunc =
   Layer <$> initFunc i h <*> randomNet h hs o initFunc
 
 backPropOneInput ::
-     Vector Double -> Vector Double -> Network -> (Network, Double)
-backPropOneInput !inp !target !net =
-  let (net', _, cost) = go inp net
+     Vector Double
+  -> Vector Double
+  -> ModelFuncs
+  -> Network
+  -> (Network, Double)
+backPropOneInput !inp !target !fs !net =
+  let (net', _, cost) = go inp fs net
    in (net', cost)
   where
-    go :: Vector Double -> Network -> (Network, Vector Double, Double)
-    go !inp (Output !c) =
+    go ::
+         Vector Double
+      -> ModelFuncs
+      -> Network
+      -> (Network, Vector Double, Double)
+    go !inp (F outA costFunc _) (Output !c) =
       let z = weightedInput c inp
-          a = sigmoid z
-          (c', dWs) = outputErrs a z inp target c
-          cost = sumElements $ (target * log a) + ((1 - target) * log (1 - a))
-          -- cost = norm_2 (target - a) ** 2
+          a = outA z
+          (c', dWs) = outputErrs inp target a z c
+          cost = costFunc target a
        in (Output c', dWs, cost)
-    go !inp (c1 `Layer` c2) =
+    go inp fs (c1 `Layer` n) =
       let (z, a) = feedForward c1 inp
-          (c1', c2', cost, dWs) = backProp c1 c2 a z inp
+          (c1', c2', cost, dWs) = backProp inp c1 fs n a z
        in (c1' `Layer` c2', dWs, cost)
     feedForward ::
          Connections -> Vector Double -> (Vector Double, Vector Double)
@@ -113,89 +88,72 @@ backPropOneInput !inp !target !net =
       -> Vector Double
       -> Connections
       -> (Connections, Vector Double)
-    outputErrs a z inp target c@(C b w bGrads wGrads)
-          -- errs = sigmoid' z * (a - target)
-     =
+    outputErrs inp target a z c@(C b w bGrads wGrads) =
       let errs = (a - target)
           errWs = errs `outer` inp
           dWs = tr w #> errs
-       in (C b w (bGrads + errs) (wGrads + errWs), dWs)
+       in (updateGrads errs errWs c, dWs)
     backProp ::
-         Connections
+         Vector Double
+      -> Connections
+      -> ModelFuncs
       -> Network
       -> Vector Double
       -> Vector Double
-      -> Vector Double
       -> (Connections, Network, Double, Vector Double)
-    backProp c1@(C b w bGrads wGrads) n@(Output _) a z inp =
-      let (n', dWs', cost) = go a n
-          errs = dWs' * sigmoid' z
+    backProp inp c1@(C b w bGrads wGrads) fs n@(Output _) a z =
+      let (n', dWs', cost) = go a fs n
+          errs = dWs'
           errWs = errs `outer` inp
           dWs = tr w #> errs
-       in (C b w (bGrads + errs) (wGrads + errWs), n', cost, dWs)
-    backProp c1@(C b w bGrads wGrads) n a z inp =
-      let (n', dWs', cost) = go a n
+       in (updateGrads errs errWs c1, n', cost, dWs)
+    backProp inp c@(C b w bGrads wGrads) fs n a z =
+      let (n', dWs', cost) = go a fs n
           errs = dWs' * relu' z
           errWs = errs `outer` inp
           dWs = tr w #> errs
-       in (C b w (bGrads + errs) (wGrads + errWs), n', cost, dWs)
+       in (updateGrads errs errWs c, n', cost, dWs)
 
-backPropagate ::
-     [Vector Double] -> [Vector Double] -> Network -> (Network, Double)
-backPropagate !inps !targets !net =
-  let (netFinal, costSum) =
-        foldl'
-          (\(net, cost) (inp, target) ->
-             let (net', cost') = backPropOneInput inp target net
-              in (net', cost + cost'))
-          (net, 0)
-          (zip inps targets)
-   in (netFinal, costSum)
-
-train ::
-     [Vector Double]
-  -> [Vector Double]
-  -> Int
-  -> Network
-  -> (Connections -> Connections)
-  -> (Network, Double)
-train !inps !targets !bs !net !optFunc =
-  let (_, _, net', costSum) = handleBatching bs inps targets net 0 optFunc
+train :: DataBunch -> ModelFuncs -> Int -> Network -> (Network, Double)
+train db@(DB !inps _) !fs !bs !net =
+  let (_, net', costSum) = handleBatching db fs net bs 0
    in (net', -(costSum / fromIntegral (length inps)))
+  where
+    handleBatching ::
+         DataBunch
+      -> ModelFuncs
+      -> Network
+      -> Int
+      -> Double
+      -> (DataBunch, Network, Double)
+    handleBatching (DB [] []) _ !net _ !cost = (DB [] [], net, cost)
+    handleBatching (DB !inps !targets) fs@(F _ _ !optFunc) !net !bs !cost =
+      let (batchInps, inps') = splitAt bs inps
+          (batchTargets, targets') = splitAt bs targets
+          (net', cost') = backPropagate (DB batchInps batchTargets) fs net
+          optimisedNet = optimise net' optFunc
+       in handleBatching (DB inps' targets') fs optimisedNet bs (cost + cost')
+    backPropagate :: DataBunch -> ModelFuncs -> Network -> (Network, Double)
+    backPropagate db@(DB inps targets) !fs !net =
+      let (netFinal, costSum) =
+            foldl'
+              (\(net, cost) (inp, target) ->
+                 let (net', cost') = backPropOneInput inp target fs net
+                  in (net', cost + cost'))
+              (net, 0)
+              (zip inps targets)
+       in (netFinal, costSum)
+    optimise :: Network -> (Connections -> Connections) -> Network
+    optimise (Output !c) !opt   = Output (opt c)
+    optimise (c `Layer` n) !opt = opt c `Layer` optimise n opt
 
-handleBatching ::
-     Int
-  -> [Vector Double]
-  -> [Vector Double]
+getAccuracy ::
+     DataBunch
+  -> Double
+  -> (Vector Double -> Vector Double)
   -> Network
   -> Double
-  -> (Connections -> Connections)
-  -> ([Vector Double], [Vector Double], Network, Double)
-handleBatching _ [] [] !net !cost _ = ([], [], net, cost)
-handleBatching !bs !inps !targets !net !cost !optFunc =
-  let (batchInps, inps') = splitAt bs inps
-      (batchTargets, targets') = splitAt bs targets
-      (net', cost') = backPropagate batchInps batchTargets net
-      optimisedNet = optimise net' optFunc
-   in handleBatching bs inps' targets' optimisedNet (cost + cost') optFunc
-
-gradientDescent :: Double -> Connections -> Connections
-gradientDescent !lr (C !b !w !bGrads !wGrads) =
-  C (b - scale lr bGrads) (w - scale lr wGrads) 0 0
-
-stochasticGradientDescent :: Int -> Double -> Connections -> Connections
-stochasticGradientDescent !bs !lr (C !b !w !bGrads !wGrads) =
-  C (b - scale lr (scale (1 / fromIntegral bs) bGrads))
-    (w - scale lr (scale (1 / fromIntegral bs) wGrads))
-    0
-    0
-
-optimise :: Network -> (Connections -> Connections) -> Network
-optimise (Output !c) !opt   = Output (opt c)
-optimise (c `Layer` n) !opt = opt c `Layer` optimise n opt
-
-getAccuracy :: [Vector Double] -> [Vector Double] -> Double -> Network -> Double
-getAccuracy !testInps !testTargets !thresh !net =
+getAccuracy (DB !inps !targets) !thresh !f !net =
   let numCorrect =
         foldl'
           (\n (inp, target) ->
@@ -204,87 +162,70 @@ getAccuracy !testInps !testTargets !thresh !net =
                      if x >= thresh
                        then 1
                        else 0)
-                  (runNet net inp) ==
+                  (runNet net inp f) ==
                 target
                then n + 1
                else n)
           0
-          (zip testInps testTargets)
-   in numCorrect / fromIntegral (length testInps)
+          (zip inps targets)
+   in numCorrect / fromIntegral (length inps)
 
 epoch ::
-     [Vector Double]
-  -> [Vector Double]
-  -> [Vector Double]
-  -> [Vector Double]
+     ModelData
+  -> ModelFuncs
   -> Int
   -> Network
-  -> (Connections -> Connections)
   -> (Network, Double, Double, Double)
-epoch !trainInps !trainTargets !testInps !testTargets !bs !net !optFunc =
-  let (net', cost) = train trainInps trainTargets bs net optFunc
-      trainAccuracy = getAccuracy trainInps trainTargets 0.5 net'
-      testAccuracy = getAccuracy testInps testTargets 0.5 net'
+epoch (D !trainData !testData) fs@(F !f _ _) !bs !net =
+  let (net', cost) = train trainData fs bs net
+      trainAccuracy = getAccuracy trainData 0.5 f net'
+      testAccuracy = getAccuracy testData 0.5 f net'
    in (net', cost / 2, trainAccuracy * 100, testAccuracy * 100)
 
-epochIO ::
-     [Vector Double]
-  -> [Vector Double]
-  -> [Vector Double]
-  -> [Vector Double]
-  -> Int
-  -> (Connections -> Connections)
-  -> IO Network
-  -> Int
-  -> IO Network
-epochIO !trainInps !trainTargets !testInps !testTargets !bs !optFunc !netIO !count = do
+epochIO :: ModelData -> ModelFuncs -> Int -> IO Network -> Int -> IO Network
+epochIO !d !f !bs !netIO !count = do
   net <- netIO
   putStrLn $ "Running epoch " ++ show count ++ "..."
-  let (net', cost, trainAcc, testAcc) =
-        epoch trainInps trainTargets testInps testTargets bs net optFunc
+  let (net', cost, trainAcc, testAcc) = epoch d f bs net
   putStrLn $ "Cost: " ++ show cost
   putStrLn $ "Train Accuracy: " ++ show trainAcc
   putStrLn $ "Test Accuracy: " ++ show testAcc
   putStrLn ""
   return net'
 
-readExams :: String -> IO ([Vector Double], [Vector Double])
-readExams fpath = do
-  inp <- readFile fpath
-  let (inps, outs) = foldr f ([], []) (lines inp)
-  return (inps, outs)
-  where
-    f :: String
-      -> ([Vector Double], [Vector Double])
-      -> ([Vector Double], [Vector Double])
-    f line (inps, outs) =
-      let (test1, line') = readScore line
-          (test2, line'') = readScore line'
-          adm = readAdm line''
-       in (vector [test1, test2] : inps, vector [adm] : outs)
-    readScore :: String -> (Double, String)
-    readScore line =
-      let (val, line') = span (/= ',') line
-       in (read val :: Double, drop 1 line')
-    readAdm :: String -> Double
-    readAdm val = read val :: Double
+data DataBunch =
+  DB
+    { inputs :: [Vector Double]
+    , labels :: [Vector Double]
+    }
+
+data ModelData =
+  D
+    { trainingData :: DataBunch
+    , testData     :: DataBunch
+    }
+
+data ModelFuncs =
+  F
+    { outputActivation :: Vector Double -> Vector Double
+    , costFunc         :: Vector Double -> Vector Double -> Double
+    , optFunc          :: Connections -> Connections
+    }
 
 examsNet :: IO ()
 examsNet = do
-  (trainInps, trainOuts) <- readExams "./data/examScoresTrain.txt"
-  (testInps, testOuts) <- readExams "./data/examScoresTest.txt"
+  (trainInps, trainOuts) <- loadExamData "./data/examScoresTrain.txt"
+  (testInps, testOuts) <- loadExamData "./data/examScoresTest.txt"
   let trainInps' = map (cmap (/ 100)) trainInps
       testInps' = map (cmap (/ 100)) testInps
+  let examsData = D (DB trainInps' trainOuts) (DB testInps' testOuts)
   let net0 = randomNet 2 [8, 8] 1 kaimingInit
   let lr = 0.01
   let bs = 64
-  let optFunc = stochasticGradientDescent bs lr
-  let epochIO' = epochIO trainInps' trainOuts testInps' testOuts bs optFunc
+  let modelFuncs = F sigmoid sigmoidCE (sgd bs lr)
+  let epochIO' = epochIO examsData modelFuncs bs
   putStrLn "Training network..."
-  trained <- foldl' epochIO' net0 [0 .. 100]
-  putStrLn "Example results from trained net..."
-  print $ runNet trained (vector [80 / 100, 80 / 100])
-  print $ runNet trained (vector [20 / 100, 30 / 100])
+  trained <- foldl' epochIO' net0 [0 .. 5]
   return ()
 
 mnistNet :: IO ()
@@ -293,11 +234,74 @@ mnistNet = do
   trainLbls <- loadMnistLabels 60000 "./data/train-labels-idx1-ubyte.gz"
   testImgs <- loadMnistImages 10000 "./data/t10k-images-idx3-ubyte.gz"
   testLbls <- loadMnistLabels 10000 "./data/t10k-labels-idx1-ubyte.gz"
+  let mnistData = D (DB trainImgs trainLbls) (DB testImgs testLbls)
   let net0 = randomNet 784 [100] 10 kaimingInit
-  let lr = 0.01
+  let lr = 0.1
   let bs = 64
-  let optFunc = stochasticGradientDescent bs lr
-  let epochIO' = epochIO trainImgs trainLbls testImgs testLbls bs optFunc
+  let modelFuncs = F softmax softmaxCE (sgd bs lr)
+  let epochIO' = epochIO mnistData modelFuncs bs
   putStrLn "Training network..."
-  trained <- foldl' epochIO' net0 [0..100]
+  trained <- foldl' epochIO' net0 [0 .. 20]
   return ()
+
+-- INIT FUNCTIONS
+randomConnections :: Int -> Int -> IO Connections
+randomConnections i o = do
+  seed1 <- randomIO :: IO Int
+  seed2 <- randomIO :: IO Int
+  let b = randomVector seed1 Uniform o * 2 - 1
+      w = uniformSample seed2 o (replicate i (-1, 1))
+  return (C b w 0 0)
+
+kaimingInit :: Int -> Int -> IO Connections
+kaimingInit i o = do
+  let b = vector (replicate o 0)
+  w <- scale (sqrt (2 / fromIntegral i)) <$> randn o i
+  return (C b w 0 0)
+
+-- ACTIVATION FUNCTIONS
+sigmoid :: Floating a => a -> a
+sigmoid x = 1 / (1 + exp (-x))
+
+sigmoid' :: Floating a => a -> a
+sigmoid' x = sigmoid x * (1 - sigmoid x)
+
+relu :: (Container c b, Ord b, Num b) => c b -> c b
+relu = cmap (max 0)
+
+relu' :: (Container c b, Ord b, Num b) => c b -> c b
+relu' =
+  cmap
+    (\x ->
+       if x <= 0
+         then 0
+         else 1)
+
+softmax :: (Container c b, Ord b, Num b, Floating b) => c b -> c b
+softmax xs = cmap (\x -> exp x / expSum) shiftxs
+  where
+    maxEle = maxElement xs
+    shiftxs = cmap (\x -> x - maxEle) xs
+    expSum = sumElements $ cmap exp shiftxs
+
+-- COST FUNCTIONS
+sigmoidCE :: Vector Double -> Vector Double -> Double
+sigmoidCE target a =
+  sumElements $ (target * log a) + ((1 - target) * log (1 - a))
+
+softmaxCE :: Vector Double -> Vector Double -> Double
+softmaxCE target a = sumElements $ target * log a
+
+-- OPTIMISATION FUNCTIONS
+gradientDescent :: Double -> Connections -> Connections
+gradientDescent !lr (C !b !w !bGrads !wGrads) =
+  C (b - scale lr bGrads) (w - scale lr wGrads) 0 0
+
+sgd :: Int -> Double -> Connections -> Connections
+sgd !bs !lr (C !b !w !bGrads !wGrads) =
+  C (b - scale lr (scale (1 / fromIntegral bs) bGrads))
+    (w - scale lr (scale (1 / fromIntegral bs) wGrads))
+    0
+    0
+
+
--- src/ReadMnist.hs
+++ src/ReadMnist.hs
@@ -1,6 +1,7 @@
 module ReadMnist
   ( loadMnistImages
   , loadMnistLabels
+  , loadExamData
   , main
   ) where
 
@@ -8,7 +9,7 @@ import           Codec.Compression.GZip (decompress)
 import qualified Data.ByteString.Lazy   as BS
 import           Data.Foldable
 import           Graphics.Image
-import           Numeric.LinearAlgebra
+import           Numeric.LinearAlgebra  as LA
 import           System.Random
 
 main :: IO ()
@@ -29,10 +30,13 @@ loadMnistImages n fpath = bytesToImgVecs n . decompress <$> BS.readFile fpath
       foldl' (\acc idx -> bytesToImgVec s idx : acc) [] [0 .. numImgs - 1]
     bytesToImgVec :: BS.ByteString -> Int -> Vector Double
     bytesToImgVec s n =
-      vector
-        (Prelude.map
-           (\x -> fromIntegral $ BS.index s (fromIntegral (n * 28 ^ 2 + 16 + x)))
-           [0 .. 783])
+      LA.scale
+        (1 / 255)
+        (vector
+           (Prelude.map
+              (\x ->
+                 fromIntegral $ BS.index s (fromIntegral (n * 28 ^ 2 + 16 + x)))
+              [0 .. 783]))
 
 loadMnistLabels :: Int -> String -> IO [Vector Double]
 loadMnistLabels n fpath = bytesToLabels n . decompress <$> BS.readFile fpath
@@ -56,3 +60,24 @@ vecToImg vec = makeImage (28, 28) (getPix vec)
   where
     getPix :: Vector Double -> (Int, Int) -> Pixel Y Double
     getPix vec (row, col) = PixelY (vec ! (row * 28 + col))
+
+loadExamData :: String -> IO ([Vector Double], [Vector Double])
+loadExamData fpath = do
+  inp <- readFile fpath
+  let (inps, outs) = foldr f ([], []) (lines inp)
+  return (inps, outs)
+  where
+    f :: String
+      -> ([Vector Double], [Vector Double])
+      -> ([Vector Double], [Vector Double])
+    f line (inps, outs) =
+      let (test1, line') = readScore line
+          (test2, line'') = readScore line'
+          adm = readAdm line''
+       in (vector [test1, test2] : inps, vector [adm] : outs)
+    readScore :: String -> (Double, String)
+    readScore line =
+      let (val, line') = span (/= ',') line
+       in (read val :: Double, drop 1 line')
+    readAdm :: String -> Double
+    readAdm val = read val :: Double
